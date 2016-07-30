e7ab75487916821398b91f7cacc6a02ebba3d62b
Fabricio Catae <fcatae@microsoft.com>
2016-07-30 11:50:39 -0700

    rollback

--- src/WebCrawler/Controllers/LinksController.cs
+++ src/WebCrawler/Controllers/LinksController.cs
@@ -41,11 +41,11 @@ namespace WebCrawler.Controllers
         {
             List<string> linkList = new List<string>();
 
-            string keyword_list = keyw.Replace("|"," AND ");
+            string keyword_list = keyw.Replace("|"," ");
 
             using (SqlConnection conn = new SqlConnection(@"Server=tcp:superbotdb.database.windows.net,1433;Initial Catalog=superbotdb;Persist Security Info=False;User ID=superbotdb;Password=P2ssw0rd@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
             {
-                SqlCommand cmd = new SqlCommand("EXEC prSearch @1", conn);
+                SqlCommand cmd = new SqlCommand("prSearch @1", conn);
                 var p1 = cmd.Parameters.Add("@1", SqlDbType.VarChar, 1000);
                 p1.Value = keyword_list;
 
